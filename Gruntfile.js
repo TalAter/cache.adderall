1250df3e096ab910950e65261462a2c8d8c70394
Tal Ater <tal@talater.com>
2016-09-01 17:26:33 +0300

    Added development workflow with babel, grunt, es2015, jshint and more

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+        'src/cache.adderall.js',
+        'Gruntfile.js'
+      ],
+      options: {
+        jshintrc: true
+      }
+    },
+    browserify: {
+      standalone: {
+        src: [ 'src/cache.adderall.js' ],
+        dest: 'dist/cache.adderall.js',
+        options: {
+          transform: [
+            [ "babelify", {presets: ["es2015"]} ]
+          ],
+          plugin: [
+            [ "minifyify", { output: "dist/cache.adderall.js.map", map: 'cache.adderall.js.map' } ],
+            [ "browserify-header" ]
+          ],
+          browserifyOptions: {
+            standalone: 'adderall',
+            debug: true
+          }
+        }
+      }
+    },
+    watch: {
+      files: ['src/cache.adderall.js', '!**/node_modules/**'],
+      tasks: ['default']
+    },
+    connect: {
+      devel: {
+        options: {
+          protocol: 'http',
+          port: 8443,
+          hostname: '*',
+          base: '.',
+          open: 'http://localhost:8443/demo'
+        }
+      }
+    }
+  });
+
+  // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Register tasks.
+  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('serve', ['default', 'connect:devel', 'watch']);
+
+};
---
+++ src/cache.adderall.js
@@ -0,0 +1,11 @@
+/*
+ <!--
+ cache.adderall()
+ version : 0.1.0
+ author  : Tal Ater @TalAter
+ license : MIT
+ https://github.com/TalAter/cache.adderall
+ -->
+ */
+
+"use strict";
