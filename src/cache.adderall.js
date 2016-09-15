f377defa5c8a91c580302d953426b179ccbfe430
Tal Ater <tal@talater.com>
2016-09-15 15:31:14 +0300

    Renamed an internal variable

--- src/cache.adderall.js
+++ src/cache.adderall.js
@@ -51,20 +51,20 @@ var addAll = function(cache, immutableRequests = [], mutableRequests = []) {
   // @TODO: Verify cache is a cache
   // @TODO: Verify immutableRequests is an array
   // @TODO: Verify mutableRequests is an array
-  var newStaticAssets = [];
+  var newImmutableRequests = [];
   return Promise.all(
     immutableRequests.map(function(url) {
       return caches.match(url).then(function(response) {
         if (response) {
           return cache.put(url, response);
         } else {
-          newStaticAssets.push(url);
+          newImmutableRequests.push(url);
           return Promise.resolve();
         }
       });
     })
   ).then(function() {
-    return cache.addAll(newStaticAssets.concat(mutableRequests));
+    return cache.addAll(newImmutableRequests.concat(mutableRequests));
   });
 };
 
