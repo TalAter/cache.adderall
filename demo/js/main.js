f59c210d4c948e1dc45a375ee85cd42d01c53c41
Tal Ater <tal@talater.com>
2016-10-12 00:04:30 +0300

    When clicking old cache.addAll() code, it is removed from DOM
    This allows the new code to be copy pasted

--- demo/js/main.js
+++ demo/js/main.js
@@ -6,6 +6,13 @@ var scrollAction = function() {
 };
 window.addEventListener('scroll', scrollAction);
 
+// Hide old addAll code sample when clicked
+var elCodeAddAll = document.getElementById("code-basic-addall");
+elCodeAddAll.addEventListener("click", function(e) {
+  elCodeAddAll.parentNode.removeChild(elCodeAddAll);
+}, false);
+
+
 // ugh... mobile hack... forgive me.
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
   vidShowAndTell.poster = "img/cache.adderall_demo_play.png"
